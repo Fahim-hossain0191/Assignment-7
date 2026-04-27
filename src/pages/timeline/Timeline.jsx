02c590b24aedf27097ea9f9d0a0218a8156073b0
Fahim <fahimhossain9121@gmail.com>
2026-04-28 04:13:28 +0600

    timeline filering done

--- src/pages/timeline/Timeline.jsx
+++ src/pages/timeline/Timeline.jsx
@@ -1,10 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 import Calls from "./Calls";
 
 const Timeline = () => {
+    const [filter, setFilter] = useState("all");
   const { timelines } = useContext(UserContext);
-
+    const filteredTimelines =
+      filter === "all"
+    ? timelines
+    : timelines.filter((item) => item.action === filter);
   return (
     <div className="w-9/12 mx-auto mt-20 space-y-3">
       <h2 className="text-3xl font-bold">Timeline</h2>
@@ -16,19 +20,24 @@ const Timeline = () => {
           tabIndex="-1"
           className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
         >
-          <li>
-            <a>Item 1</a>
+            <li>
+            <button onClick={() => setFilter("all")}>All</button>
           </li>
           <li>
-            <a>Item 2</a>
+             <button onClick={() => setFilter("call")}>Call</button>
+          </li>
+          <li>
+            <button onClick={() => setFilter("text")}>Text</button>
+          </li>
+          <li>
+            <button onClick={() => setFilter("video")}>Video</button>
           </li>
         </ul>
       </div>
-      <div className=" my-3">
-      {timelines.map((user, index) => (
-        <Calls user={user} key={index} />
-      ))}
-
+       <div className="my-3">
+        {filteredTimelines.map((user, index) => (
+          <Calls user={user} key={index} />
+        ))}
       </div>
     </div>
   );
