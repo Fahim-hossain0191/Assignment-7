af990b1fbc21076f4d7eab36c513937c22a209e8
Fahim <fahimhossain9121@gmail.com>
2026-04-27 08:11:34 +0600

    navbar done

--- src/component/shared/navbar/Navbar.jsx
+++ src/component/shared/navbar/Navbar.jsx
@@ -1,26 +1,52 @@
 import React from "react";
-
+import { RiHome2Line } from "react-icons/ri";
+import { NavLink } from "react-router";
+import { RiTimeLine } from "react-icons/ri";
+import { IoStatsChartSharp } from "react-icons/io5";
+import Image from "../../../assets/logo.png"
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    
+        
+    <div className="navbar bg-base-100 shadow-sm  px-10">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">KeenKeeper</a>
+        <img src={Image} alt="Logo" />
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Home</a>
+              <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  && "bg-[#244D3F] text-white rounded-lg"
+                  
+              }
+            >
+              <RiHome2Line />
+              Home
+            </NavLink>
           </li>
           <li>
-            <a>Timeline</a>
+           
+            <NavLink to='/timeline' className={({isActive})=>
+            isActive&&"bg-[#244D3F] text-white rounded-lg"
+            }>
+                 <RiTimeLine />  Timeline
+            </NavLink>
           </li>
-          <li>
-            <a>Link</a>
+          <li >
+              <NavLink to='/stats' className={({isActive})=>
+            isActive&&"bg-[#244D3F] text-white rounded-lg"
+            }>
+                <IoStatsChartSharp /> Stats
+            </NavLink>
           </li>
           
         </ul>
       </div>
     </div>
+ 
   );
 };
 
