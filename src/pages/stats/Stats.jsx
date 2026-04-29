b54099ee2e86ba078d9cd540479da9ec9d6bf42b
Fahim <fahimhossain9121@gmail.com>
2026-04-29 22:12:16 +0600

    footer section done

--- src/pages/stats/Stats.jsx
+++ src/pages/stats/Stats.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
+import { UserContext } from "../../context/UserContext";
 
 const Stats = () => {
-    return (
-        <div>
-            <h2>Hello from stats section</h2>
-        </div>
-    );
+  const { timelines } = useContext(UserContext);
+
+  const textCount = timelines.filter(
+    item => item.action === "text"
+  ).length;
+
+  const callCount = timelines.filter(
+    item => item.action === "call"
+  ).length;
+
+  const videoCount = timelines.filter(
+    item => item.action === "video"
+  ).length;
+
+  const data = [
+    { name: "Text", value: textCount, fill: "#7C3AED" },
+    { name: "Call", value: callCount, fill: "#134E4A" },
+    { name: "Video", value: videoCount, fill: "#22C55E" },
+  ];
+
+  return (
+    <div className=" my-10">
+      <div >
+              <h2 className="ml-57 text-3xl font-bold mb-4">Friendship Analytics</h2>
+      </div>
+      
+      <div className="w-8/12 mx-auto  shadow-lg">
+      <div >
+      <h2 className="m-2 text-xl font-medium">By Interaction Type</h2>
+
+      </div>
+      <div className="flex justify-center">
+
+      <PieChart width={500} height={400}>
+        <Pie
+          data={data}
+          innerRadius={90}
+          outerRadius={130}
+          cornerRadius={10}
+          paddingAngle={5}
+          dataKey="value"
+        />
+
+        <Legend />
+        <Tooltip />
+      </PieChart>
+      </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
